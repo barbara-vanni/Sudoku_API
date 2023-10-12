56cca6f6b2c121368030ff4699469c40ba369bf2
Dhjali <barbara.vanni@laplateforme.io>
2023-10-12 19:25:23 +0200

    styling buttons

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // Access to API for the grid datas
-let grid = fetch ('http://31.33.247.37:3000/api/sudoku')
-.then(response => response.json())
-.then(date=> {
-    console.log(data);
-    return date;
-})
-// Use data to fill the grid
-// surement faire une petite bouboucle 
-.catch(err => {
+let grid = fetch('http://31.33.247.37:3000/api/sudoku')
+    .then(response => response.json())
+    .then(date => {
+        const sudokuGrid = document.getElementById("sudoku");
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                const cellValue = data[i][j];
+                const cell = sudokuGrid.rows[i].cells[j].querySelector('input');
+                if (cellValue !== 0) {
+                    cell.value = cellValue;
+                    cell.readOnly = true;
+                }
+            }
+        }
+        startTimer();
+        console.log(data);
+        return date;
+    })
+.catch (err => {
     console.log(err);
-})
+});
 
 
 
-startTimer();
 
 
 
